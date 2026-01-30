e4599185f7efc903a6aeb6dd92d37630d3ef8c45
Alan <Alan>
2026-01-30 21:02:02 +0500

    main file

--- app/main.py
+++ app/main.py
@@ -1,23 +1,12 @@
-import sys
-import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
 from app.lifespan import lifespan
 from app.api.v1 import router as v1_router
-
-
-from fastapi.staticfiles import StaticFiles
 
 
 def create_app() -> FastAPI:
     app = FastAPI(root_path="/api", lifespan=lifespan)
     
-    app.mount("/admin", StaticFiles(directory="app/static/admin", html=True), name="admin")
-
     app.include_router(v1_router)
 
     app.add_middleware(
